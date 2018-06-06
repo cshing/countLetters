f946e3322a921215fbb2cf26ac1be37d3a31e01a
Carmen Shing <carmenshing@gmail.com>
2018-06-06 19:15:37 +0000

    clarified i iterators

--- countLetters.js
+++ countLetters.js
@@ -2,11 +2,11 @@ function countLetters(strArray){
     var returnObj = {};
     for (var i = 0; i < strArray.length; i++){
       noSpaces = strArray[i].split(' ').join('');
-      for (var ii = 0; ii < noSpaces.length; ii++){
-        if (returnObj[noSpaces.charAt(ii)] === undefined){
-          returnObj[noSpaces.charAt(ii)] = 1;
+      for (var x = 0; x < noSpaces.length; x++){
+        if (returnObj[noSpaces.charAt(x)] === undefined){
+          returnObj[noSpaces.charAt(x)] = 1;
         }else{
-          returnObj[noSpaces.charAt(ii)] += 1;
+          returnObj[noSpaces.charAt(x)] += 1;
         }
       }
     }
