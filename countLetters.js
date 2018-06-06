6f24ee6fa74427c7b2a88ae90d163f7958cbfcb8
Carmen Shing <carmenshing@gmail.com>
2018-06-06 19:43:31 +0000

    removed spaces before for loop

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,7 @@
 function countLetters(strArray){
     var returnObj = {};
-    for (var i = 0; i < strArray.length; i++){
-      noSpaces = strArray[i].split(' ').join('');
+    var noSpaces = strArray.join('').split(' ').join('');
+
       for (var x = 0; x < noSpaces.length; x++){
         if (returnObj[noSpaces.charAt(x)] === undefined){
           returnObj[noSpaces.charAt(x)] = 1;
